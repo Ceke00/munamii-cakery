a3bce40aa8574f9aa7243641859d249e40222a11
Cecilia Egevad <ceciliaegevad@gmail.com>
2024-10-28 16:52:27 +0100

    title on pages

--- src/Pages/Confirmation.js
+++ src/Pages/Confirmation.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function Confirmation() {
-	const location = useLocation();
-	const queryParams = new URLSearchParams(location.search);
-	const firstName = queryParams.get("firstName");
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const firstName = queryParams.get("firstName");
 
-	return (
-		<div className="p-10">
-			<h1>Thank you {firstName}!</h1>
-			<p>We have received your message and will get back to you shortly.</p>
-			<p>
-				Back{" "}
-				<Link className="underline decoration-1 underline-offset-4 hover:bg-lightpink " to="/">
-					to home page.
-				</Link>
-			</p>
-		</div>
-	);
+  return (
+    <div className="p-10">
+      <Helmet>
+        <title>Munamii Cakery - Confirmation</title>
+      </Helmet>
+      <h1>Thank you {firstName}!</h1>
+      <p>We have received your message and will get back to you shortly.</p>
+      <p>
+        Back{" "}
+        <Link
+          className="underline decoration-1 underline-offset-4 hover:bg-lightpink "
+          to="/"
+        >
+          to home page.
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default Confirmation;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,46 +2,68 @@ import React from "react";
 import { Link } from "react-router-dom";
 import weddingCake from "../Images/wedding_cake_pink.png";
 /* import munamii_baking from "../Images/munamii_smiles.png"; */
+import { Helmet } from "react-helmet";
 
 function Home() {
-	return (
-		<header>
-			<div className="relative isolate overflow-hidden bg-darkgreen px-6 pt-16 shadow-2xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-				<svg
-					viewBox="0 0 1024 1024"
-					aria-hidden="true"
-					className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0">
-					<circle r={512} cx={512} cy={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
-					<defs>
-						<radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-							<stop stopColor="#30ab95" />
-							<stop offset={1} stopColor="#aadce6" />
-						</radialGradient>
-					</defs>
-				</svg>
-				<div className="mx-auto max-w-md  lg:mx-0 lg:flex-auto lg:py-32 text-left">
-					<h1 className="text-balance text-white">Munamii Cakery</h1>
-					<p className=" text-pretty text-lg/8 text-white">
-						Welcome to Munamii Cakery! We specialize in creating delightful cupcakes and exquisite wedding cakes
-						that add a touch of sweetness to your special moments.
-					</p>
-					<div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-						<Link
-							to="/products"
-							className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-darkgreen shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
-							Products
-						</Link>
-						<Link to="/about" className="text-sm font-semibold leading-6 text-white">
-							About Munamii <span aria-hidden="true">→</span>
-						</Link>
-					</div>
-				</div>
-				<div className="relative mt-16 h-80 lg:mt-8">
-					<img alt="Exclusive wedding cake with roses and gold decorations" src={weddingCake} className="absolute -left-60 top-0 w-[57rem] max-w-none" />
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <>
+      <Helmet>
+        <title>Munamii Cakery - Home Page</title>
+      </Helmet>
+      <header>
+        <div className="relative isolate overflow-hidden bg-darkgreen px-6 pt-16 shadow-2xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <svg
+            viewBox="0 0 1024 1024"
+            aria-hidden="true"
+            className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+          >
+            <circle
+              r={512}
+              cx={512}
+              cy={512}
+              fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+              fillOpacity="0.7"
+            />
+            <defs>
+              <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#30ab95" />
+                <stop offset={1} stopColor="#aadce6" />
+              </radialGradient>
+            </defs>
+          </svg>
+          <div className="mx-auto max-w-md  lg:mx-0 lg:flex-auto lg:py-32 text-left">
+            <h1 className="text-balance text-white">Munamii Cakery</h1>
+            <p className=" text-pretty text-lg/8 text-white">
+              Welcome to Munamii Cakery! We specialize in creating delightful
+              cupcakes and exquisite wedding cakes that add a touch of sweetness
+              to your special moments.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <Link
+                to="/products"
+                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-darkgreen shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                Products
+              </Link>
+              <Link
+                to="/about"
+                className="text-sm font-semibold leading-6 text-white"
+              >
+                About Munamii <span aria-hidden="true">→</span>
+              </Link>
+            </div>
+          </div>
+          <div className="relative mt-16 h-80 lg:mt-8">
+            <img
+              alt="Exclusive wedding cake with roses and gold decorations"
+              src={weddingCake}
+              className="absolute -left-60 top-0 w-[57rem] max-w-none"
+            />
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Home;
