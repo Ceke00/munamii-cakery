f7e8da27d08982c70e459fd502481833af8d192d
Cecilia Egevad <ceciliaegevad@gmail.com>
2024-10-29 11:14:46 +0100

    change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
@@ -12,22 +12,25 @@ import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
-    <div className="App">
-      <ScrollToTop />
-      <NavBar />
-      <div className="container max-w-screen-lg mx-auto bg-white">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/products" element={<Products />}></Route>
+    <Router>
+      <div className="App">
+        <ScrollToTop />
+        <NavBar />
+        <div className="container max-w-screen-lg mx-auto bg-white">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/products" element={<Products />}></Route>
 
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/confirmation" element={<Confirmation />}></Route>
-          <Route path="/*" element={<ErrorMessage />}></Route>
-        </Routes>
-        <Footer />
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/confirmation" element={<Confirmation />}></Route>
+            <Route path="/*" element={<ErrorMessage />}></Route>
+          </Routes>
+
+          <Footer />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
